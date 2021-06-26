238bd3175ccf8b9e233d9df5587aa44280a5e9f3
sravancipher <83302891+sravancipher@users.noreply.github.com>
2021-06-26 22:03:40 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright By ShapeAi @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div classname="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic web dev React Js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
